7d69ec2a6b2c3dbb9bc1b9589c7e45bd918a6c7c
Miguels <adolfolaguna007@gmail.com>
2021-08-20 15:05:23 -0400

    Updates

---
+++ precache-manifest.f20c99c8aec089fc25a84b48f567a779.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5d993cb2cf218ea355b3f91e06bd6e3",
+    "url": "/React-Food-Search/index.html"
+  },
+  {
+    "revision": "5da5ea5b705a05977036",
+    "url": "/React-Food-Search/static/css/main.093836e1.chunk.css"
+  },
+  {
+    "revision": "5aee017f9bfcac57195e",
+    "url": "/React-Food-Search/static/js/2.f1bc050c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/React-Food-Search/static/js/2.f1bc050c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5da5ea5b705a05977036",
+    "url": "/React-Food-Search/static/js/main.d0796a40.chunk.js"
+  },
+  {
+    "revision": "cdc973e6438f2f836b3d",
+    "url": "/React-Food-Search/static/js/runtime-main.c17a6035.js"
+  }
+]);
